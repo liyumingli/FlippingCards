785441ce829302bd7e117fd9d6fb765a8643ba65
Yuming Li <t00180267@365s.ittralee.ie>
2016-11-15 15:50:02 +0000

    Added card flipping

--- src/FlippingCards.java
+++ src/FlippingCards.java
@@ -2,8 +2,13 @@
  * Created by t00180267 on 15/11/2016.
  */
 import javax.swing.*;
+import java.awt.*;
 
 public class FlippingCards extends JFrame {
+    private int rows = 4;
+    private int cols = 4;
+    private int numCards = rows * cols;
+    private Card[] cards = new Card[numCards];
 
     public static void main(String[] args) {
         FlippingCards mainFrame = new FlippingCards();
@@ -14,7 +19,24 @@ public class FlippingCards extends JFrame {
     public FlippingCards () {
         setTitle("Card Flipping Game");
 
-        setSize(600, 600);
+        setSize(850, 900);
+
+        setLocationRelativeTo(null);
+
+        Container imageContainer = new Container();
+
+        imageContainer.setLayout(new GridBagLayout());
+        add(imageContainer);
+
+        GridBagConstraints c = new GridBagConstraints();
+
+        for (int i = 0;i < numCards; i++) {
+            c.gridx = i % cols;
+            c.gridy = i / rows;
+            cards[i] = new Card("pic1.jpg");
+
+            imageContainer.add(cards[i], c);
+        }
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
