63bd7385262a998a5ed77e6a299785ab0c16cccd
Yuming Li <t00180267@365s.ittralee.ie>
2016-11-17 10:57:12 +0000

    Added random card creation

--- src/FlippingCards.java
+++ src/FlippingCards.java
@@ -3,12 +3,15 @@
  */
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class FlippingCards extends JFrame {
     private int rows = 4;
     private int cols = 4;
     private int numCards = rows * cols;
     private Card[] cards = new Card[numCards];
+    private int[] randomIndexes = getRandomIntSequence();
 
     public static void main(String[] args) {
         FlippingCards mainFrame = new FlippingCards();
@@ -31,14 +34,35 @@ public class FlippingCards extends JFrame {
         GridBagConstraints c = new GridBagConstraints();
 
         for (int i = 0;i < numCards; i++) {
-            c.gridx = i % cols;
-            c.gridy = i / rows;
-            cards[i] = new Card(this, "pic1.jpg");
+            c.gridx = randomIndexes[i] % cols;
+            c.gridy = randomIndexes[i] / rows;
+
+
+            cards[i] = new Card(this, "pic" + (int)Math.ceil((i + 1) / 2d) + ".jpg");
 
             imageContainer.add(cards[i], c);
         }
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    private int[] getRandomIntSequence () {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 0; i < numCards; i++) {
+            list.add(i);
+        }
+
+        Collections.shuffle(list);
+
+        Integer[] array1 = list.toArray(new Integer[list.size()]);
+
+        int[] array2 = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            array2[i] = list.get(i);
+        }
+
+        return array2;
     }
 
     public void cardClicked (Card whichCard) {
