977a694b8e5c1cdf9e8fde135e1f9ad629531ed1
liyumingli <l_yuming@yahoo.ie>
2016-11-29 13:56:16 +0000

    select

--- src/FlippingCards.java
+++ src/FlippingCards.java
@@ -10,8 +10,9 @@ import java.util.Collections;
 import java.lang.Thread;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.CardLayout;
 
-public class FlippingCards extends JFrame {
+public class FlippingCards extends JFrame implements ActionListener {
     private int rows = 4;
     private int cols = 4;
 //    private int NumCard;
@@ -21,7 +22,10 @@ public class FlippingCards extends JFrame {
     private boolean matched=false;
     private Card c1;
     private Card c2;
-    private Timer t;
+//    private Timer t;
+    JPanel card;
+    JButton button1, button2, button3;
+    final static String BUTTONPANEL = "Card with JButtons";
 
     public static void main(String[] args) {
         FlippingCards mainFrame = new FlippingCards();
@@ -43,6 +47,32 @@ public class FlippingCards extends JFrame {
 
         GridBagConstraints c = new GridBagConstraints();
 
+        button1 = new JButton("Easy");
+        button2 = new JButton("Normal");
+        button3 = new JButton("Hard");
+
+        button1.addActionListener(this);
+        button2.addActionListener(this);
+        button3.addActionListener(this);
+
+        JPanel card1 = new JPanel();
+        card1.add(button1);
+        JPanel card2 = new JPanel();
+        card2.add(button2);
+        JPanel card3 = new JPanel();
+        card3.add(button3);
+
+
+        card = new JPanel(new CardLayout());
+
+        card.add(card1);
+        card.add(card2);
+        card.add(card3);
+        card.add(card1, BUTTONPANEL);
+
+        getContentPane().add(card);
+
+
         for (int i = 0;i < numCards; i++) {
             c.gridx = randomIndexes[i] % cols;
             c.gridy = randomIndexes[i] / rows;
@@ -51,9 +81,31 @@ public class FlippingCards extends JFrame {
             cards[i] = new Card(this, "pic" + (int)Math.ceil((i + 1) / 2d) + ".jpg");
 
             imageContainer.add(cards[i], c);
+
         }
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+    public void actionPerformed(ActionEvent e)
+    {
+        if (e.getSource() == button1){
+            CardLayout cardLayout = (CardLayout) card.getLayout();
+            rows = 4;
+            cols = 4;
+            cardLayout.next(card);
+        }
+          else if (e.getSource() == button2){
+            CardLayout cardLayout = (CardLayout) card.getLayout();
+            rows = 6;
+            cols = 6;
+            cardLayout.next(card);
+        }
+        else{
+            CardLayout cardLayout = (CardLayout) card.getLayout();
+            rows = 8;
+            cols = 8;
+            cardLayout.next(card);
+        }
     }
 
     private int[] getRandomIntSequence () {
@@ -136,38 +188,39 @@ public class FlippingCards extends JFrame {
             }
         }
     }
-    public class CountUpProgressBar extends JPanel {
 
-        private JProgressBar bar = new JProgressBar(JProgressBar.HORIZONTAL, 0, 100);
-        private JLabel label = new JLabel("", JLabel.CENTER);
-        private Timer timer = new Timer(100, new ActionListener() {
-
-            private int counter = 1;
-            public void actionPerformed(ActionEvent ae) {
-                label.setText(String.valueOf(counter));
-                bar.setValue(++counter);
-                if (counter > 100) {
-                    timer.stop();
-                }
-            }
-        });
-
-        CountUpProgressBar() {
-            super.setLayout(new GridLayout(0, 1));
-            bar.setValue(0);
-            timer.start();
-            this.add(bar);
-            this.add(label);
-            JOptionPane.showMessageDialog(null, this);
-        }
-
-        public void main(String[] args) {
-            SwingUtilities.invokeLater(new Runnable() {
-
-                public void run() {
-                    CountUpProgressBar cdpb = new CountUpProgressBar();
-                }
-            });
-        }
-    }
+//    public class CountUpProgressBar extends JPanel {
+//
+//        private JProgressBar bar = new JProgressBar(JProgressBar.HORIZONTAL, 0, 100);
+//        private JLabel label = new JLabel("", JLabel.CENTER);
+//        private Timer timer = new Timer(100, new ActionListener() {
+//
+//            private int counter = 1;
+//            public void actionPerformed(ActionEvent ae) {
+//                label.setText(String.valueOf(counter));
+//                bar.setValue(++counter);
+//                if (counter > 100) {
+//                    timer.stop();
+//                }
+//            }
+//        });
+//
+//        CountUpProgressBar() {
+//            super.setLayout(new GridLayout(0, 1));
+//            bar.setValue(0);
+//            timer.start();
+//            this.add(bar);
+//            this.add(label);
+//            JOptionPane.showMessageDialog(null, this);
+//        }
+//
+//        public void main(String[] args) {
+//            SwingUtilities.invokeLater(new Runnable() {
+//
+//                public void run() {
+//                    CountUpProgressBar cdpb = new CountUpProgressBar();
+//                }
+//            });
+//        }
+//    }
 }
